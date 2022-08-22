01f9904dee3c52f0bf13de8879db9b4e74cea528
achallenger1234 <achallenger1234@gmail.com>
2022-08-22 07:16:37 +0000

    user auth

--- src/hooks/Login/useAuth.ts
+++ src/hooks/Login/useAuth.ts
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import { useNavigate } from "react-router-dom"
 
+import axios from "axios";
+
 import { useLoginUser, LoginUser } from "../../hooks/Providers/useLoginUserProvider";
+
+import { LoginAuthUser } from "../../types/user";
 
 export const useAuth = () => {
     const [loading, setLoading] = useState(false)
     const navigate = useNavigate();
     const { setLoginUser } = useLoginUser();
-    const login = (email: string, password: string) => {
+    const login = useCallback((email: string, password: string) => {
         setLoading(true);
-        navigate('blog');
-        setLoading(false);
-        const loginUser: LoginUser = {
-            email
-        }
-        setLoginUser(loginUser);
-    }
+        axios.get<LoginAuthUser>(`//jsonplaceholder.typicode.com/users/${email}`)
+        .then( async res  => { 
+            if (res.data && password === '1') {
+                navigate('blog');
+                const loginUser: LoginUser = {
+                    email
+                }
+                setLoginUser(loginUser);          
+            } else { 
+                console.log("missing");
+            }
+
+        })
+        .catch( () => {console.log("missing");} )
+        .finally( () => setLoading(false) )
+        
+    }, [])
     
     
     return { login, loading } 
--- src/types/user.ts
+++ src/types/user.ts
@@ -2,3 +2,27 @@ export type User = {
   email: string;
   password: string;
 };
+
+export type LoginAuthUser = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
